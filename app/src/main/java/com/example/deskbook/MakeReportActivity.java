4c4d37e6e0ee64cc852dafc8e28e2293e3aa4b76
adam razak <adambk04@gmail.com>
2020-05-09 03:49:42 +0800

    added send report function 2.0 completed

--- app/src/main/java/com/example/deskbook/MakeReportActivity.java
+++ app/src/main/java/com/example/deskbook/MakeReportActivity.java
@@ -1,27 +1,69 @@
 package com.example.deskbook;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.UUID;
 
 public class MakeReportActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_CODE = 1000 ;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    private final int PICK_IMAGE_REQUEST = 71;
+    FirebaseStorage storage;
+    StorageReference storageReference;
     FirebaseDatabase database;
     FirebaseAuth firebaseAuth;
     DatabaseReference dbref, dbref2;
     FirebaseUser user;
-    String userID;
-    Button btnSendReport, btnSnapImage, btnChooseImage;
+    Uri imageUri;
+    String userID, date, time, desc, reportImage, reportImageName, workspaceName, userImage, userName;
+    int checkPic = 0;
+    Button btnSendReport ;
+    ImageButton btnSnapImage, btnChooseImage;
+    ImageView ivReport;
     EditText etReport;
-    String workspaceName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +78,179 @@ public class MakeReportActivity extends AppCompatActivity {
         user = firebaseAuth.getCurrentUser();
         userID = user.getUid();
 
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
         database = FirebaseDatabase.getInstance();
         dbref = database.getReference("/users/" + userID);
         dbref2 = database.getReference("/report");
 
+        etReport = findViewById(R.id.ETreport);
+        ivReport = findViewById(R.id.IVreport);
+        btnChooseImage = findViewById(R.id.BtnChooseReportImg);
+        btnSnapImage = findViewById(R.id.BtnTakePicture);
+        btnSendReport = findViewById(R.id.BtnMakeReport);
 
+        imageUri = null;
 
+        btnSnapImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if OS build > marshmellow, request permission to use camera
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        // permission not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        // show popup to request for permission
+                        requestPermissions(permission, PERMISSION_CODE);
 
+                    }
+                    // permission is already granted
+                    else{
+                        openCamera();
+                    }
+                }
+            }
+        });
+
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnSendReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String timeStamp = new SimpleDateFormat("dd/MM/yyyy-HH:mm:ss").format(Calendar.getInstance().getTime());
+                String[] parts = timeStamp.split("-");
+                date = parts[0];
+                time = parts[1];
+                desc = etReport.getText().toString();
+                dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        userImage = user.getProfilePic();
+                        userName = user.getName();
+                        uploadImage();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void uploadImage() {
+        //if user choose a report  picture
+        if(imageUri != null){
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Sending Report...");
+            progressDialog.show();
+
+            final StorageReference ref = storageReference.child("ReportImage/" + UUID.randomUUID().toString());
+            ref.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MakeReportActivity.this, "Report Submitted", Toast.LENGTH_SHORT).show();
+
+                            Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!uri.isComplete()) ;
+                            Uri url = uri.getResult();
+                            String pictureUrl = url.toString();
+                            String pictureName = url.getLastPathSegment();
+                            System.out.println("Picture download link : " + pictureUrl);
+                            System.out.println("Picture name : " + pictureName);
+
+                            storeReport(pictureUrl, pictureName);
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MakeReportActivity.this, "Submission Fail" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Submitting " + (int) progress + " %");
+                        }
+                    });
+        }
+        //if user does not choose a report picture
+        else{
+            storeReport(null, null);
+            finish();
+        }
+    }
+
+    private void storeReport(String pictureUrl, String pictureName){
+        String key = dbref2.push().getKey();
+        Report report = new Report(workspaceName, date, desc, pictureUrl, pictureName, userImage, time, userID, userName);
+        dbref2.child(key).setValue(report);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    private void chooseImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the Camera");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        // Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                }
+                else {
+                    Toast.makeText(MakeReportActivity.this, "Permission Denied ....", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // called when image is captured
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            ivReport.setVisibility(View.VISIBLE);
+            ivReport.setImageURI(imageUri);
+        }
+        // called when image is chosen
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            ivReport.setVisibility(View.VISIBLE);
+            ivReport.setImageURI(imageUri);
+        }
     }
 }
--- app/src/main/java/com/example/deskbook/Report.java
+++ app/src/main/java/com/example/deskbook/Report.java
@@ -1,15 +1,15 @@
 package com.example.deskbook;
 
 public class Report {
-    private String workspaceName;
-    private String date;
-    private String desc;
-    private String image;
-    private String name;
-    private String profilePic;
-    private String time;
-    private String uid;
-    private String userName;
+    public String workspaceName;
+    public String date;
+    public String desc;
+    public String image;
+    public String name;
+    public String profilePic;
+    public String time;
+    public String uid;
+    public String userName;
 
     public Report(String workspaceName, String date, String desc, String image, String name, String profilePic, String time, String uid, String userName) {
         this.workspaceName = workspaceName;
