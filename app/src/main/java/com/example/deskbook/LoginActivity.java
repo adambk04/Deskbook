507d0fff0126a4224f019be6a5f65f47adbf5545
adam razak <adambk04@gmail.com>
2020-05-12 19:07:32 +0800

    styled login and signup activity

--- app/src/main/java/com/example/deskbook/LoginActivity.java
+++ app/src/main/java/com/example/deskbook/LoginActivity.java
@@ -15,6 +15,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.nio.LongBuffer;
+
 public class LoginActivity extends AppCompatActivity {
 
     public Button btnLogin;
